1fa9e5eb45bf8be7565d89338fe66ac989ffa0d2
sb <sb@koertner-muth.com>
2018-09-23 07:12:20 +0200

    Receive successfully messages send from the C based driver

--- ui/Common/MqttBackgroundService.cs
+++ ui/Common/MqttBackgroundService.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Net.Mqtt;
+using System.Reactive.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -40,7 +43,43 @@ namespace ui.Common
         {
             _mqttClient = await CreateClient();
             _logger.LogInformation("Execute");
-            await Task.CompletedTask;
+            
+            
+            var sessionState = await Connect(_mqttClient, _brokerConfig.User);
+            
+            await _mqttClient.SubscribeAsync(_brokerConfig.Topic, MqttQualityOfService.ExactlyOnce); //QoS2
+            
+            _mqttClient
+                .MessageStream
+                .Subscribe(msg => Console.WriteLine($"Message received in topic {msg.Topic}"));
+  	
+            _mqttClient
+                .MessageStream
+                .Where(msg => msg.Topic == _brokerConfig.Topic)
+                .Subscribe(LogMessage);
+
+            // sends a initial message on the topic
+            await Publish(_mqttClient, _brokerConfig.Topic, "Hello from C#", MqttQualityOfService.ExactlyOnce);     
+         
+            
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                // This eShopOnContainers method is querying a database table 
+                // and publishing events into the Event Bus (RabbitMS / ServiceBus)
+                // CheckConfirmedGracePeriodOrders();
+
+                await Task.Delay(5000, stoppingToken);
+            }
+            
+            _logger.LogDebug($"GracePeriod background task is stopping.");
+            
+            
+            //Method to unsubscribe a topic or many topics, which means that the message will no longer
+            //be received in the MessageStream anymore
+            await _mqttClient.UnsubscribeAsync(_brokerConfig.Topic);
+            
+            await Disconnect(_mqttClient);
         }
 
         public override Task StartAsync(CancellationToken cancellationToken)
@@ -57,6 +96,29 @@ namespace ui.Common
             _mqttServer.Stop();
             _mqttServer.Dispose();
             return base.StopAsync(cancellationToken);
+        }
+        
+        private void LogMessage(MqttApplicationMessage msg)
+        {
+            _logger.LogInformation($"Message received in topic {msg.Topic}");
+            _logger.LogInformation($"´{Encoding.UTF8.GetString(msg.Payload)}´");
+        }
+
+        private async Task<SessionState> Connect(IMqttClient client, string clientId)
+        {
+            var sessionState = await client.ConnectAsync(new MqttClientCredentials(clientId), cleanSession: true);
+            return sessionState;
+        }
+
+        private async Task Disconnect(IMqttClient client)
+        {
+            await client.DisconnectAsync();
+        }
+        
+        private async Task Publish(IMqttClient client, string topic, string payload, MqttQualityOfService qos)
+        {
+            var message = new MqttApplicationMessage(topic, Encoding.UTF8.GetBytes(payload));
+            await client.PublishAsync(message, qos);
         }
     }
 }
