d15c86df9308d230703ddb3961daba006adeab20
sb <sb@koertner-muth.com>
2018-12-16 14:31:35 +0100

    Responsive Door Image and Container

--- ui/ClientApp/src/components/Constants.jsx
+++ ui/ClientApp/src/components/Constants.jsx
@@ -7,5 +7,43 @@ export const arrowOpen = (
 	<span className="arrow-open" />
 )
 
+// Constructs a Svg Mask depending of media query size 
+
+// <ellipse cx={width/2} cy={height/2} rx={width/2} ry={height/1.5} fill="black" filter="url(#filter2)"/>
+
+export const SvgMask = ({ size, imgUrl }) => {
+	var width = 1400; 
+	var height = 700;
+	
+	if (size === "l") {
+		width = 1170; 
+		height = 585;
+	} else if (size === "m") {
+		width = 962; 
+		height = 481;
+	} else if (size === "s") {
+		width = 738; 
+		height = 369;
+	}
+	 return (
+			 <svg
+				width={width} height={height} 
+					 xmlns="http://www.w3.org/2000/svg"
+					 xlink="http://www.w3.org/1999/xlink"
+			 >
+					 <defs>
+						 <filter id="filter2">
+					 <feGaussianBlur stdDeviation="20"></feGaussianBlur>
+				 </filter>
+					 </defs>
+			
+				<image width={width} height={height} xlinkHref={imgUrl} > */}
+				</image>
+			 </svg>
+	 );
+};
+
+
+
 
 
--- ui/ClientApp/src/components/Door.jsx
+++ ui/ClientApp/src/components/Door.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
 
+import MediaQuery from 'react-responsive';
+
+import { SvgMask } from './Constants';
+
+// <Row className="door" style={{backgroundImage: `url(${imgUrl})`}}>
+// </Row> 
+
 const Door = ({ doorId, isOpen }) => {
 	let doorState = isOpen ? 'open' : 'close'
 
 	var imgUrl = '/images/doors/' + doorId + '_' + doorState + '.png'
 
 	return (
-		<Row className="door" style={{backgroundImage: `url(${imgUrl})`}}>
-		</Row>
+		<div>
+			<MediaQuery minWidth={1430}>
+				<SvgMask size="xl" imgUrl={imgUrl} />
+			</MediaQuery>
+			<MediaQuery minWidth={1200} maxWidth={1429}>
+				<SvgMask size="l" imgUrl={imgUrl} />
+			</MediaQuery>
+			<MediaQuery minWidth={992} maxWidth={1199}>
+				<SvgMask size="m" imgUrl={imgUrl} />
+			</MediaQuery>
+			<MediaQuery maxWidth={991}>
+				<SvgMask size="s" imgUrl={imgUrl} />
+			</MediaQuery>
+		</div>
+
 	);
-}; 
+};
 
 export default Door;
