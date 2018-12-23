e4c96cb9b4ef295a610dfd7cbfe16735f81359e1
sb <sb@koertner-muth.com>
2018-12-23 06:57:58 +0100

    Einfärbung der Türen analog zu den Räumen

--- ui/ClientApp/src/components/GroundPlan.tsx
+++ ui/ClientApp/src/components/GroundPlan.tsx
@@ -18,43 +18,22 @@ export class GroundPlan extends React.PureComponent<GroundPlanProps> {
 	public static defaultProps = {
 	};
 
-	public state = {
-		room: this.props.selectedRoom,
-	};
-
 	public handleRoomSelect = (e: React.MouseEvent<SVGElement>) => {
 		this.props.onClick(e.currentTarget.id);
-		this.setState({ room: e.currentTarget.id });
 	};
 
 	private getClassName = (prefix: string, roomId: string) => {
-		return prefix + ' ' + ((this.state.room === roomId) ? 'on' : 'off');
+		return prefix + ' ' + ((this.props.selectedRoom === roomId) ? 'on' : 'off');
 	}
 
 	public render() {
-		/*const roomStyle = {
-			stroke: 'black',
-			strokeOpacity: 1,
-			strokeWidth: 8,
-			strokeLinecap: 'butt',
-			strokeLinejoin: 'miter',
-			strokeDasharray: 'none',
-		}
-	
-		const textStyle = {
-			fontFamily: 'Berlin',
-			fontSize: '2.5em',
-			stroke: '#dcdcdc',
-			fill: 'black'
-		}
-	*/
 
 		return (
 			<svg width="100%" height="100%" version="1.1" viewBox="25 0 1083 687">
 				<g id="property">
-					<text x="501" y="645">Tor West</text>
 					<rect id='W1' className={this.getClassName('property', 'W1')} onClick={this.handleRoomSelect} x="28" y="14" width="1077" height="666" />
-					<line x1="680" y1="680" x2="453" y2="680" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+					<line className={this.getClassName('roomdoor', 'W1')} x1="680" y1="680" x2="453" y2="680"  />
+					<text x="501" y="645">Tor West</text>
 				</g>
 				<g id="production">
 					<text x="733" y="45">Production</text>
@@ -64,22 +43,22 @@ export class GroundPlan extends React.PureComponent<GroundPlanProps> {
 					<rect id='204' className={this.getClassName('room','204')} onClick={this.handleRoomSelect} x="737" y="369" width="326" height="156" />
 					<rect id='205' className={this.getClassName('room','205')} onClick={this.handleRoomSelect} x="737" y="71" width="326" height="156" />
 					<g>
-						<line x1="566.9291" y1="113.3858" x2="566.9291" y2="170.0787" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
-						<line x1="566.9291" y1="136.063" x2="566.9291" y2="138.8976" opacity="1" stroke="#999998" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+						<line className={this.getClassName('roomdoor', '200')} x1="567" y1="110" x2="567" y2="170" />
+						<line className='roomdoor center' x1="567" y1="136" x2="567" y2="144" />
 					</g>
 					<g>
-						<line x1="566.9291" y1="269.2914" x2="566.9291" y2="325.9843" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
-						<line x1="566.9291" y1="291.9685" x2="566.9291" y2="294.8031" opacity="1" stroke="#999998" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+						<line className={this.getClassName('roomdoor', '201')} x1="567" y1="270" x2="567" y2="330" />
+						<line className='roomdoor center' x1="567" y1="296" x2="567" y2="304" />
 					</g>
 					<g>
-						<line x1="566.9291" y1="425.1968" x2="566.9291" y2="481.8898" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
-						<line x1="566.9291" y1="447.874" x2="566.9291" y2="450.7086" opacity="1" stroke="#999998" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+						<line className={this.getClassName('roomdoor', '202')} x1="567" y1="420" x2="567" y2="480" />
+						<line className='roomdoor center' x1="567" y1="446" x2="567" y2="454" />
 					</g>
 					<g>
-						<line x1="1062.992" y1="99.2126" x2="1062.992" y2="198.4252" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
-						<line x1="1062.992" y1="138.8976" x2="1062.992" y2="143.8583" opacity="1" stroke="#999998" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+						<line className={this.getClassName('roomdoor', '205')} x1="1063" y1="99" x2="1063" y2="199" />
+						<line className='roomdoor center' x1="1063" y1="145" x2="1063" y2="153" />
 					</g>
-					<line x1="1062.992" y1="396.8504" x2="1062.992" y2="496.063" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+					<line className={this.getClassName('roomdoor', '204')} x1="1063" y1="397" x2="1063" y2="496" />
 				</g>
 				<g id="administration">
 					<text x="192" y="45">Verwaltung</text>
@@ -93,8 +72,8 @@ export class GroundPlan extends React.PureComponent<GroundPlanProps> {
 						d="M311.811 496.063 L467.7165 496.063 L467.7165 595.2756 L311.811 609.4488 Z " />
 
 					<g>
-						<line x1="212.5984" y1="99.2126" x2="212.5984" y2="155.9055" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
-						<line x1="212.5984" y1="121.8897" x2="212.5984" y2="124.7244" opacity="1" stroke="#999998" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+						<line x1="213" y1="99" x2="213" y2="156" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+						<line x1="213" y1="122" x2="213" y2="125" opacity="1" stroke="#999998" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
 					</g>
 					<g>
 						<line x1="311.8813" y1="99.28291" x2="311.8813" y2="155.9758" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
@@ -112,7 +91,7 @@ export class GroundPlan extends React.PureComponent<GroundPlanProps> {
 						<line x1="311.811" y1="425.1968" x2="311.811" y2="481.8898" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
 						<line x1="311.811" y1="447.874" x2="311.811" y2="450.7086" opacity="1" stroke="#999998" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
 					</g>
-					<line x1="311.7993" y1="508.8788" x2="311.7993" y2="537.2252" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
+					<line x1="312" y1="509" x2="312" y2="537" className={this.getClassName('roomdoor', '102')} />
 					<g transform="rotate(90 262.2685 552.5645)">
 						<line x1="262.2685" y1="519.9661" x2="262.2685" y2="585.1629" opacity="1" stroke="#E6E6E5" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
 						<line x1="262.2685" y1="546.0449" x2="262.2685" y2="549.3046" opacity="1" stroke="#999998" strokeOpacity="1" strokeWidth="10" strokeLinecap="butt" strokeLinejoin="miter" strokeDasharray="none" />
