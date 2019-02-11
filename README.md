f82de935d4f9c67c9c992cc739f9cf6e1a113cbd
sb <sb@koertner-muth.com>
2019-02-11 20:06:15 +0100

    Entität 'Key' samt Test hinzugefügt

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ LOCKBASE Electronic Lock Interface Demo
 
 The SubDirectorys are 
 
-* /ui - WebApp
+* /coredomain - Main Business Logic of the application
+* /tests - Unit Test's for CoreDomain
+* /ui  - WebApp
 * /driver - LowLevel driver connected to the Web-App (written in C)
 * /doc - Lockbase ELI specification and other documents
 * /sample - Empty DLL body and other samples
---
+++ tests/CoreDomain/KeyEntity.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit; 
+
+using Lockbase.CoreDomain.Entities; 
+
+namespace Lockbase.ui.UnitTest.CoreDomain {
+
+	public class KeyTest {
+
+		[Fact]
+		public void TestKeyCreation() {
+			Key entity = new Key("000000hqvs1lo","103-1",null, "Fender, Klaus");
+			Assert.Equal("000000hqvs1lo", entity.Id);
+			Assert.Equal("103-1", entity.Name);
+			Assert.Null(entity.AppId);
+			Assert.Equal("Fender, Klaus", entity.ExtData);
+		}
+
+		[Fact]
+		public void TestKeyIdMandatory() {
+			Assert.Throws<ArgumentNullException>(() => new Key(string.Empty,"103-1",null,"Fender, Klaus"));
+			Assert.Throws<ArgumentNullException>(() => new Key(null,"103-1",null,"Fender, Klaus"));
+		}
+	}
+}
