c23ada825de142cdc6a0a70e33d1c03a6c167ef3
sb <sb@koertner-muth.com>
2019-03-24 07:48:17 +0100

    Zuordnung von Schlüsseln zu Schlössern bzw. umgekehrt

--- coredomain/ValueObjects/PolicyAssignment.cs
+++ coredomain/ValueObjects/PolicyAssignment.cs
@@ -1,15 +1,61 @@
 
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using Lockbase.CoreDomain.Entities;
 
 namespace Lockbase.CoreDomain.ValueObjects  {
+
+
+    public class Assignment<TMaster,TDetail> : IEquatable<Assignment<TMaster,TDetail>> 
+        where TMaster : IEquatable<TMaster>
+        where TDetail : IEquatable<TDetail> 
+    {
+        public TMaster Master { get; private set; }
+
+        public ISet<TDetail> Details { get; private set; }
+
+        public Assignment(TMaster master, IEnumerable<TDetail> details)
+        {
+            Master = master;
+
+            Details = details.Aggregate( 
+					ImmutableHashSet<TDetail>.Empty, (accu, current) => accu.Add(current) );
+        }
+
+        public bool Equals(Assignment<TMaster,TDetail> other)
+        {
+            if (other == null)
+                return false;
+
+            if (this.Master.Equals(other.Master) && this.Details.SetEquals(other.Details))
+                return true;
+             else
+                 return false;
+        }
+    }
+    public class LockAssignment : Assignment<Lock,Key> {
+        
+        public LockAssignment(Lock @lock, IEnumerable<Key> keys):base(@lock, keys) 
+        {
+        }
+    }
     
+    public class KeyAssignment : Assignment<Key,Lock> {
+        
+        public KeyAssignment(Key key, IEnumerable<Lock> locks):base(key, locks) 
+        {
+        }
+    }
+    
+
     public class PolicyAssignment : IEquatable<PolicyAssignment> {
             
         public AccessPolicy Source { get; private set; }
-        public Either<Lock,Key> Target { get; private set; }
+        public Either<LockAssignment,KeyAssignment> Target { get; private set; }
 
-        public PolicyAssignment(AccessPolicy source, Either<Lock,Key> target) {
+        public PolicyAssignment(AccessPolicy source, Either<LockAssignment,KeyAssignment> target) {
             this.Source = source;
             this.Target = target;
         }
@@ -19,10 +65,7 @@ namespace Lockbase.CoreDomain.ValueObjects  {
             if (other == null)
                 return false;
 
-            if (this.Source == other.Source && this.Target == other.Target)
-                return true;
-             else
-                 return false;
+            return (this.Source.Equals(other.Source) && this.Target.Equals(other.Target));
         }
     }
 }
