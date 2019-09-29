bd7a83217648b7544b03f6488b895fef63979d4c
sb <sb@koertner-muth.com>
2019-09-29 07:34:37 +0200

    unit test noch auf netcore 2.2

--- backend/coredomain/EnumerableExtensions.cs
+++ backend/coredomain/EnumerableExtensions.cs
@@ -1,10 +1,9 @@
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 
 namespace Lockbase.CoreDomain
 {
-	public static class EnumerableExtensions
+    public static class EnumerableExtensions
 	{
 		// https://stackoverflow.com/questions/1577822/passing-a-single-item-as-ienumerablet
 		public static IEnumerable<T> Yield<T>(this T item) => Enumerable.Repeat(item, 1);
